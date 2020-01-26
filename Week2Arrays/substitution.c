0d0741539e95f993c44f8f3f8a2b303ed364bf2f
Erik Oja <erik_oja@yahoo.com>
2020-01-25 21:06:37 -0600

    passing all tests

--- Week2Arrays/substitution.c
+++ Week2Arrays/substitution.c
@@ -1,48 +1,51 @@
-//substitution cipher, user enters key of 26 letters, then message, output is encrypted per key
-
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-bool check_argv_1(string str);
 string to_lower(string str);
+bool check_argv_1(string str);
+int checkif_duplicates(string str, char x);
 int alpha_index(char x);
 
 string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 int main(int argc, string argv[])
 {
-    if (argc == 2 && check_argv_1(argv[1])) 
+    if (argc == 1)
     {
-        //printf("good\n");
+        printf("You must enter one key 26 letter characters in lenght with no spaces\n");
+        return 1;
+    }
+    string key = to_lower(argv[1]);
+    int len = strlen(argv[1]);
+
+    if (argc == 2 && check_argv_1(key))
+    {
         string text = get_string("plaintext: ");
-        int len = strlen(argv[1]);
-        string key = to_lower(argv[1]);
+        int textlen = strlen(text);
+        
         //convert key into array of ascii numbers
-        int key_arr[strlen(key)];
-        for (int i = 0; i < strlen(key); i++)
+        int key_arr[len];
+        for (int i = 0; i < len; i++)
         {
             key_arr[i] = key[i];
-            //printf("key: %i\n", key_arr[i]);
         }
-        for (int j = 0; j < len; j++)
-        {   
+        
+        //map text to key
+        for (int j = 0; j < textlen; j++)
+        {
             //check if it is a letter
             if (isupper(text[j]) || islower(text[j]))
             {
+                int index = alpha_index(text[j]);
                 if (isupper(text[j]))
                 {
-                    int index = alpha_index(text[j]);
-                    //printf("UC text[j] %c, key[index]: %i\n", text[j], key[index]);
-                    //printf("index: %i, key[index] - 32: %i\n", index, key[index] - 32);
                     text[j] = key[index] - 32;
                 }
                 else
                 {
-                    int index = alpha_index(text[j]);
-                    //printf("else text[j] %c, key[index]: %i\n", text[j], key[index]);
-                    text[j] = (char) key[index];
+                    text[j] = key[index];
                 }
             }
             //if not a letter leave as is
@@ -59,28 +62,7 @@ int main(int argc, string argv[])
         printf("Key must only be 26 letter characters in lenght with no spaces\n");
         return 1;
     }
-}
 
-//make sure only letters entered
-bool check_argv_1(string str)
-{
-    bool result = true;
-    int len = strlen(str);
-    int arr[len];
-    for (int i = 0; i < len; i++)
-    {
-        if ((isupper(str[i]) || islower(str[i])) && strlen(str) == 26)
-        {
-            arr[i] = 
-            continue;
-        }
-        else
-        {
-            result = false;
-            break;
-        }
-    }
-    return result;
 }
 string to_lower(string str)
 {
@@ -94,9 +76,42 @@ string to_lower(string str)
         else
         {
             continue;
-        }  
+        }
     }
     return str;
+}
+bool check_argv_1(string str)
+{
+    bool result = true;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        int count = checkif_duplicates(str, str[i]);
+        if (strlen(str) == 26 && islower(str[i]) && count <= 1)
+        {
+            continue;
+        }
+        else
+        {
+            result = false;
+            printf("break\n");
+            break;
+        }
+    }
+    return result;
+}
+int checkif_duplicates(string str, char x)
+{
+    int count = 0;
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == x)
+        {
+            count++;
+        }
+    }
+    return count;
 }
 int alpha_index(char x)
 {
