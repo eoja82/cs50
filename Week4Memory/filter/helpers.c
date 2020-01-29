8fb20c1d12a33ab5cfa005f000eab162d96d04ec
Erik Oja <erik_oja@yahoo.com>
2020-01-29 16:17:41 -0600

    sepia function working

--- Week4Memory/filter/helpers.c
+++ Week4Memory/filter/helpers.c
@@ -5,13 +5,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    printf("%i, %i, %i\n", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
+    // printf("%i, %i, %i\n", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
     //need to convert to float to get decimal when dividing
     float ave = (float) (image[0][0].rgbtBlue + image[0][0].rgbtGreen + image[0][0].rgbtRed) / 3;
-    printf("ave: %.3f\n", ave);
+    //printf("ave: %.3f\n", ave);
     int nc = round(ave);
-    /* int new_color = average - floor(average) > 0.5 ? ceil(average) : floor(average); */
-    printf("new_color: %i\n", nc);
+    // int new_color = average - floor(average) > 0.5 ? ceil(average) : floor(average);
+    //printf("new_color: %i\n", nc);
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -24,13 +24,34 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = new_color;
         }
     }
-    printf("%i, %i, %i\n", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
+    //printf("%i, %i, %i\n", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
+{   
+    /* printf("%i, %i, %i\n", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
+    int b = image[0][0].rgbtBlue;
+    int g = image[0][0].rgbtGreen;
+    int r = image[0][0].rgbtRed;
+    int newr = round((.393 * r) + (.769 * g) + (.189 * b));
+    printf("newr: %i\n", newr); */
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int blue = image[i][j].rgbtBlue;
+            int green = image[i][j].rgbtGreen;
+            int red = image[i][j].rgbtRed;
+            int new_blue = round((.272 * red) + (.534 * green) + (.131 * blue));
+            int new_green = round((.349 * red) + (.686 * green) + (.168 * blue));
+            int new_red = round((.393 * red) + (.769 * green) + (.189 * blue));
+            image[i][j].rgbtBlue = new_blue < 255 ? new_blue : 255;
+            image[i][j].rgbtGreen = new_green < 255 ? new_green : 255;
+            image[i][j].rgbtRed = new_red < 255 ? new_red : 255;
+        }
+    }
     return;
 }
 
