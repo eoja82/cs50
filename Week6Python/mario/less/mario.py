09ae090e383a07d698dab23e8a8074d9369dca96
Erik Oja <erik_oja@yahoo.com>
2020-02-05 20:21:05 -0600

    passing tests

---
+++ Week6Python/mario/less/mario.py
@@ -0,0 +1,17 @@
+""" print a hash pyramid like the end of Mario Bros with height and width between 1 and 8 based on user input """
+""" cs50 library required for this file """
+from cs50 import get_int
+
+height = 0
+while height < 1 or height > 8:
+    height = get_int("Height: ")
+
+h = height
+w = 1
+
+while w < height + 1:
+    print(" " * (h - 1), end = "")
+    print("#" * w)
+    h -= 1
+    w += 1
+
