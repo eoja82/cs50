f94cab276db17d63162fc00c8e657447d30917de
Erik Oja <erik_oja@yahoo.com>
2020-02-07 15:57:59 -0600

    working large and small

--- Week6Python/dna/dna.py
+++ Week6Python/dna/dna.py
@@ -7,63 +7,45 @@ def main():
         print("Usage: python dna.py file1 file2")
         exit(1)
 
-    strs = {"AGAT": 0, "AATG": 0, "TATC": 0, "none": 0}
-    matches = 0
-    matched = "none"
+    strs = {"AGATC": 0, "TTTTTTCT": 0, "AATG": 0, "TCTAG": 0, "GATA": 0, "TATC": 0, "GAAA": 0, "TCTG": 0}
+    keys = strs.keys()
+    print(keys)
+    print(len(keys[6]))
 
-    """ tally up matches """
     with open(argv[2], "r") as seqFile:
         sequence = seqFile.read()
-        frm = 0
-        to = 4
-        while to < len(sequence):
-            """ get to start of STRs """
-            if sequence[frm:to] not in ["AGAT", "AATG", "TATC"]:
-                frm += 1
-                to += 1
-                continue
-            if sequence[frm:to] == "AGAT":
-                if matched != "AGAT":
-                    strs[matched] = matches if matches > strs[matched] else strs[matched]
-                    matched = "AGAT"
-                    matches = 1
-                else:
+        for key in keys:
+            frm = 0
+            to = len(key)
+            matches = 0
+            while to < len(sequence):
+                if sequence[frm:to] != key:
+                    if matches != 0:
+                        strs[key] = matches if matches > strs[key] else strs[key]
+                    """ print(" no match ", frm, to) """
+                    matches = 0
+                    frm += 1
+                    to += 1
+                    continue
+                if sequence[frm:to] == key:
+                    """ print("match ", frm, to, matches) """
                     matches += 1
-                frm += 4
-                to += 4
-            elif sequence[frm:to] == "AATG":
-                if matched != "AATG":
-                    strs[matched] = matches if matches > strs[matched] else strs[matched]
-                    matched = "AATG"
-                    matches = 1
-                else:
-                    matches += 1
-                frm += 4
-                to += 4
-            elif sequence[frm:to] == "TATC":
-                if matched != "TATC":
-                    strs[matched] = matches if matches > strs[matched] else strs[matched]
-                    matched = "TATC"
-                    matches = 1
-                else:
-                    matches += 1
-                frm += 4
-                to += 4
-            else:
-                frm += 4
-                to += 4
-        """ input values of what is in matches and matched in strs after the loop """
-        strs[matched] = matches if matches > strs[matched] else strs[matched]
+                    frm += len(key)
+                    to += len(key)
+            strs[key] = matches if matches > strs[key] else strs[key]
+    print(strs.items())
+
+    
 
     person = "No match"
     """ open csv file, database of people, to check for a match """
     with open(argv[1], "r") as data:
         db = csv.DictReader(data)
         for row in db:
-            if int(row["AGAT"]) == strs["AGAT"] and int(row["AATG"]) == strs["AATG"] and int(row["TATC"]) == strs["TATC"]:
+            if int(row["AGATC"]) == strs["AGATC"] and int(row["AATG"]) == strs["AATG"] and int(row["TATC"]) == strs["TATC"]:
                 person = row["name"]
                 
     print(person)
 
-    
+
 main()
