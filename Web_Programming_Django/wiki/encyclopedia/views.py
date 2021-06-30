3c8e5f602fd838ad8c6484199ae9f4f796359e89
Erik Oja <erik_oja@yahoo.com>
2021-06-30 13:24:57 -0500

    renders markdown files

--- Web_Programming_Django/wiki/encyclopedia/views.py
+++ Web_Programming_Django/wiki/encyclopedia/views.py
@@ -1,7 +1,8 @@
+import markdown
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -9,6 +10,13 @@ def index(request):
     })
 
 def wikipage(request, wiki):
-    return render(request, "encyclopedia/wikipage.html", {
-        "wiki": wiki.capitalize(), "file": util.get_entry(wiki.lower())
-    })
+    entry = util.get_entry(wiki.lower())
+    if entry != None:
+        md = markdown.Markdown().convert(entry)
+        title = md[md.index(">") + 1:]
+        title = title[:title.index("<")]
+        return render(request, "encyclopedia/wikipage.html", {
+            "file": md, "title": title
+        })
+    else:
+        return HttpResponseNotFound(f"<h1 style='text-align: center; margin: 3rem;'>Sorry, there is no information available for \"{wiki.lower()}\".</h1>")
