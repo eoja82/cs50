afee3597f7aebd0ed50e3e5942dc61509818ff8d
Erik Oja <erik_oja@yahoo.com>
2021-07-02 17:47:21 -0500

    add path for wikiEdit

--- Web_Programming_Django/wiki/encyclopedia/urls.py
+++ Web_Programming_Django/wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:wiki>", views.wikipage, name="wikipage"),
-    path("newWiki/", views.newWiki, name="newWiki")
+    path("newWiki/", views.newWiki, name="newWiki"),
+    path("wikiEdit/<str:wiki>", views.wikiEdit, name="wikiEdit")
 ]
