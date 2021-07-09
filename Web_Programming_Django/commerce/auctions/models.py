b51b30803c586b6153a0e71501c117557190e16a
Erik Oja <erik_oja@yahoo.com>
2021-07-09 14:06:36 -0500

    add user to Listings and __str__ to models

--- Web_Programming_Django/commerce/auctions/models.py
+++ Web_Programming_Django/commerce/auctions/models.py
@@ -16,6 +16,7 @@ class Listings(models.Model):
         ("Watercraft", "Watercraft"),
         ("Electronics", "Electronics")
     ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=2000)
     photoURL = models.URLField(blank=True)
@@ -23,15 +24,23 @@ class Listings(models.Model):
     starting_bid = models.DecimalField(max_digits=12, decimal_places=2)
     date_created = models.DateField()
 
+    def __str__(self):
+        return f"User: {self.user}, Listing: {self.title}."
+
 class Bids(models.Model):
     listing = models.ForeignKey(Listings, on_delete=models.CASCADE) # related_name
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     bid = models.DecimalField(max_digits=12, decimal_places=2)
-    bidder = models.CharField(max_length=100)
     date_created = models.DateField()
+
+    def __str__(self):
+        return f"User: {self.user}, Listing: {self.listing}, Bid: {self.bid}"
 
 class Comments(models.Model):
     listing = models.ForeignKey(Listings, on_delete=models.CASCADE) # related_name
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(max_length=1000)
     date_created = models.DateField()
+
+    def __str__(self):
+        return f"User: {self.user}, Comment: {self.comment}, Date: {self.date_created}"
