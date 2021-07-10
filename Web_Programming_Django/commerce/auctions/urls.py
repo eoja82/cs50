faf02a602302d36a29022a47cd5a6afdcf15ff34
Erik Oja <erik_oja@yahoo.com>
2021-07-10 13:15:15 -0500

    add paths to create listing & view single listing

--- Web_Programming_Django/commerce/auctions/urls.py
+++ Web_Programming_Django/commerce/auctions/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("auction/new", views.new_auction, name="new_auction")
+    path("auction/new", views.new_auction, name="new_auction"),
+    path("listings/<int:listing_id>", views.listing_view, name="listing_view")
 ]
