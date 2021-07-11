fb3d309eaf845502773d98cc3b8043165e2b6fa0
Erik Oja <erik_oja@yahoo.com>
2021-07-11 16:03:27 -0500

    register models

--- Web_Programming_Django/commerce/auctions/admin.py
+++ Web_Programming_Django/commerce/auctions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Bid, Comment, Listings, User
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Listings)
+admin.site.register(Bid)
+admin.site.register(Comment)
