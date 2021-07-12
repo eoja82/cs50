615715ba9dc0a0359f8fce4b4aa940af7e5a50a1
Erik Oja <erik_oja@yahoo.com>
2021-07-12 16:08:18 -0500

    add paths for categories

--- Web_Programming_Django/commerce/auctions/urls.py
+++ Web_Programming_Django/commerce/auctions/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("auction/new", views.new_auction, name="new_auction"),
     path("listings/<int:listing_id>", views.listing_view, name="listing_view"),
-    path("listings/<int:listing_id>/watch", views.watch, name="watch")
+    path("listings/<int:listing_id>/watch", views.watch, name="watch"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category>", views.category_view, name="category_view")
 ]
