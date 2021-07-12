ab1e526687cd46187fe2d8ec3076f8747fb659b4
Erik Oja <erik_oja@yahoo.com>
2021-07-12 16:02:47 -0500

    register Watch model

--- Web_Programming_Django/commerce/auctions/admin.py
+++ Web_Programming_Django/commerce/auctions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Bid, Comment, Listings, User
+from .models import Bid, Comment, Listings, User, Watch
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Listings)
 admin.site.register(Bid)
 admin.site.register(Comment)
+admin.site.register(Watch)
