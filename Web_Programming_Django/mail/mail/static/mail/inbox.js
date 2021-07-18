9793f97b73c15e00a64f262bf183d5c7a3422010
Erik Oja <erik_oja@yahoo.com>
2021-07-18 14:05:46 -0500

    add send email

--- Web_Programming_Django/mail/mail/static/mail/inbox.js
+++ Web_Programming_Django/mail/mail/static/mail/inbox.js
@@ -20,6 +20,35 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector("#compose-form").addEventListener("submit", sendEmail)
+
+  function sendEmail(e) {
+    const data = e.target.elements,
+          recipients = data["compose-recipients"].value,
+          subjet = data["compose-subject"].value,
+          body = data["compose-body"].value
+
+    fetch("/emails", {
+      method: "POST",
+      body: JSON.stringify({
+        recipients: recipients,
+        subjet: subjet,
+        body: body
+      })
+    })
+    .then( res => {
+      if (res.ok) {
+        console.log("Message Sent")
+        load_mailbox("sent")
+      } else {
+        alert("Something went wrong.  The message was not sent.")
+      }
+    })
+    .catch( err => console.log("Error:", err))
+
+    e.preventDefault()
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +59,6 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  
 }
