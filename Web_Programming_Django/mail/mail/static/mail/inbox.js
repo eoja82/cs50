170c739aa63320ef957c6faaff8958fc6aab51bc
Erik Oja <erik_oja@yahoo.com>
2021-07-18 19:19:58 -0500

    display email list

--- Web_Programming_Django/mail/mail/static/mail/inbox.js
+++ Web_Programming_Django/mail/mail/static/mail/inbox.js
@@ -26,14 +26,14 @@ function compose_email() {
   function sendEmail(e) {
     const data = e.target.elements,
           recipients = data["compose-recipients"].value,
-          subjet = data["compose-subject"].value,
+          subject = data["compose-subject"].value,
           body = data["compose-body"].value
 
     fetch("/emails", {
       method: "POST",
       body: JSON.stringify({
         recipients: recipients,
-        subjet: subjet,
+        subject: subject,
         body: body
       })
     })
@@ -60,5 +60,59 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  
+  fetch(`/emails/${mailbox}`)
+  .then( res => res.json())
+  .then( emails => {
+    emails.forEach( email => {
+      const emailDiv = document.getElementById("emails-view"),
+        card = document.createElement("div"),
+        cardBody = document.createElement("div"),
+        cardTitle = document.createElement("h5"),
+        cardSubtitle = document.createElement("h6"),
+        preview = document.createElement("P"),
+        date = document.createElement("p"),
+        read = document.createElement("div")
+      console.log(email.subject)
+      card.className = "card"
+      card.style.cursor = "pointer"
+      card.onclick = () => view_email(email)
+      
+      cardBody.className = "card-body"
+      cardBody.style.position = "relative"
+      card.append(cardBody)
+      
+      cardTitle.className = "card-title"
+      cardTitle.textContent = email.recipients[0]
+      cardBody.append(cardTitle)
+      
+      cardSubtitle.className = "card-subtitle"
+      cardSubtitle.textContent = email.subject
+      cardBody.append(cardSubtitle)
+      
+      preview.className = "card-text text-muted"
+      preview.textContent = email.body.slice(0, 50)
+      cardBody.append(preview)
+      
+      date.className = "card-text"
+      if (!email.read) {
+        date.style = "position: absolute; bottom: 18px; right: 20px"
+      } else {
+        date.style = "position: absolute; bottom: 35px; right: 20px"
+      }
+      date.textContent = email.timestamp
+      cardBody.append(date)
+
+      if (!email.read) {
+        read.style = "width: 16px; height: 16px; background-color: #007bff; border-radius: 8px; position: absolute; top: 20px; right: 20px;"
+        cardBody.append(read)
+      }
+
+      emailDiv.append(card)
+    })
+  })
+  .catch( err => console.log("Error: ", err))
+}
+
+function view_email(email) {
+  console.log(email)
 }
