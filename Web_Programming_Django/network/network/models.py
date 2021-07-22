01f4d2cd697d4f34e16ac0a421adab22d70c242e
Erik Oja <erik_oja@yahoo.com>
2021-07-22 16:56:11 -0500

    add Post model

--- Web_Programming_Django/network/network/models.py
+++ Web_Programming_Django/network/network/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    post = models.CharField(max_length=250)
+    date_created = models.DateField(auto_now_add=True)
