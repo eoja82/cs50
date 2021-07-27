205ef0125bda1a3df548a2d366a0643796e31248
Erik Oja <erik_oja@yahoo.com>
2021-07-27 14:02:33 -0500

    change profile/<str:user> to profile/<str:profile>

--- Web_Programming_Django/network/network/urls.py
+++ Web_Programming_Django/network/network/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("posts/like", views.like_post, name="like"),
     path("post", views.post, name="post"),
-    path("profile/<str:user>", views.profile, name="profile"),
+    path("profile/<str:profile>", views.profile, name="profile"),
     path("register", views.register, name="register")
 ]
