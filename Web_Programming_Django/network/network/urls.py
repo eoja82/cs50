5d2569d11b15915175990da193799b33e3ae45d5
Erik Oja <erik_oja@yahoo.com>
2021-07-27 18:53:31 -0500

    add path following

--- Web_Programming_Django/network/network/urls.py
+++ Web_Programming_Django/network/network/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("following/", views.following, name="following"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("posts/like", views.like_post, name="like"),
